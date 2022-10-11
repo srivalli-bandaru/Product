3d01b797abc2cfb29df63c2ac090a4213e831d09
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-10-11 15:01:48 +0530

    commit for prime

--- junit/src/main/java/Com/Prime.java
+++ junit/src/main/java/Com/Prime.java
@@ -4,7 +4,7 @@ public class Prime {
 	static boolean prime(int n) {
 		boolean b=false;
 		int f=0;
-		//try {
+		
 		for(int i=1;i<=n/2;i++) {
 			if(n%i==0)
 				f++;
@@ -16,5 +16,10 @@ public class Prime {
 	    
 		return b;
 }
-}
 
+
+public static void main(String args[]) {
+	if (prime(3))
+		System.out.println("true");
+}
+}
